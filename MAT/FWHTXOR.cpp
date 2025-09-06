cf47bbfb266e374ca84c46939dbd671ccc22b561
leoanholetto1 <leogomesanholetto@gmail.com>
2025-09-06 00:59:46 -0300

    Correção

--- MAT/FWHTXOR.cpp
+++ MAT/FWHTXOR.cpp
@@ -1,3 +1,5 @@
+//No caso de contar todos subarray xor, use o array freq[preix_xor] e faça a convolução com ele mesmo. Alem disso, trate o zero separado.
+
 void fwht_xor(vector<int> &a) {
     int n = a.size();
     for (int s = 2, h = 1; s <= n; s <<= 1, h <<= 1) {
@@ -36,4 +38,31 @@ vector<int> conv_xor(vector<int> a, vector<int> b) {
     for (int i = 0; i < n; i++) a[i] *= b[i];
     ifwht_xor(a);
     return a;
+}
+
+//Tem a segunda, por que a primeira ja me enganou em alguns problemas!
+void fwht_xor(vt<ll> &a) {
+    int n = a.size();
+    for(int len = 2; len <= n; len <<= 1){
+        for(int i = 0; i < n; i += len){
+            for(int j = 0; j < len/2; j++){
+                ll u = a[i + j], v = a[i + j + len/2];
+                a[i + j] = u + v;
+                a[i + j + len/2] = u - v;
+            }
+        }
+    }
+}
+ 
+void ifwht_xor(vt<ll> &a) {
+    int n = a.size();
+    for(int len = 2; len <= n; len <<= 1){
+        for(int i = 0; i < n; i += len){
+            for(int j = 0; j < len/2; j++){
+                ll u = a[i + j], v = a[i + j + len/2];
+                a[i + j] = (u + v) / 2;
+                a[i + j + len/2] = (u - v) / 2;
+            }
+        }
+    }
 }
